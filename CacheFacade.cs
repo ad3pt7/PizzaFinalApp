ffd04621eee442c5677d3760e229be85cd5d6e00
denisstorozhev1 <denisstorozhev1@yandex.ru>
2022-11-14 21:12:10 +0500

    Bug fixes

---
+++ CacheFacade.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Caching;
+
+namespace PizzaFinalApp
+{
+    public static class CacheFacade
+    {
+        private static ObjectCache _cache;
+
+        static CacheFacade()
+        {
+            _cache = MemoreCache.Default;
+        }
+
+        public static void Set(string key,object data)
+        {
+            _cache.Set(key, data, null);
+        }
+
+        public static object GetObject(string key)
+        {
+            return _cache[key];
+        }
+
+        public static void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+    }
+}
